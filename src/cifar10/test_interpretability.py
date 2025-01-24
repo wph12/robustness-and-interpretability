feat: add script for robustbench pre-trained models feat: add cosine similarity with saliency and integrated gradient to 'quantify' interpretability fix: some bugs with previous interpretability thing

src/cifar10/test_interpretability.py
@@ -2,11 +2,11 @@ import torch
 import logging
 import torchvision
 import torchvision.transforms as transforms
-import torchvision.transforms.functional as TF
+import torch.nn.functional as F
 
 from torchvision import models
 
-from captum.attr import IntegratedGradients
+from captum.attr import IntegratedGradients, Saliency
 
 def integrated_gradient(model, dataloader, run_id):
     logger = logging.getLogger(run_id)
@@ -26,11 +26,38 @@ def integrated_gradient(model, dataloader, run_id):
         attributions_flat = attributions.view(attributions.size(0), -1)  # Flatten attributions
         
         # Step 5: Compute Cosine Similarity
-        similarity = TF.cosine_similarity(images_flat, attributions_flat, dim=1)
+        similarity = F.cosine_similarity(images_flat, attributions_flat, dim=1)
         cosine_similarities.extend(similarity.tolist())
 
     cosine_similarities = torch.tensor(cosine_similarities)
     mean_similarity = cosine_similarities.mean().item()
 
-    print(f"Mean Cosine Similarity: {mean_similarity:.4f}")
-    logger.info(f"Mean Cosine Similarity: {mean_similarity:.4f}")
+    print(f"Mean Cosine Similarity (IG): {mean_similarity:.4f}")
+    logger.info(f"Mean Cosine Similarity (IG): {mean_similarity:.4f}")
+
+def saliency(model, dataloader, run_id):
+    logger = logging.getLogger(run_id)
+
+    cosine_similarities = []
+    model.zero_grad()
+    sal = Saliency(model)
+
+    for images, labels in dataloader:
+        # Compute attributions
+        attributions = sal.attribute(
+            images, baselines=torch.zeros_like(images), target=labels
+        )
+        
+        # Step 4: Flatten Inputs and Attributions
+        images_flat = images.view(images.size(0), -1)  # Flatten each image into 1D
+        attributions_flat = attributions.view(attributions.size(0), -1)  # Flatten attributions
+        
+        # Step 5: Compute Cosine Similarity
+        similarity = F.cosine_similarity(images_flat, attributions_flat, dim=1)
+        cosine_similarities.extend(similarity.tolist())
+
+    cosine_similarities = torch.tensor(cosine_similarities)
+    mean_similarity = cosine_similarities.mean().item()
+
+    print(f"Mean Cosine Similarity (Saliency): {mean_similarity:.4f}")
+    logger.info(f"Mean Cosine Similarity (Saliency): {mean_similarity:.4f}")