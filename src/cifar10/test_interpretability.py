8fff115cd142034d0f10762a1e899b6249012cdf
WPH <peiheweng@gmail.com>
2025-02-12 10:28:59 +0800

    feat: add functions for saliency and ig metrics

--- src/cifar10/test_interpretability.py
+++ src/cifar10/test_interpretability.py
@@ -3,6 +3,7 @@ import logging
 import torchvision
 import torchvision.transforms as transforms
 import torch.nn.functional as F
+import numpy as np
 
 from torchvision import models
 
@@ -55,13 +56,37 @@ def alignment(model, dataloader, run_id, device):
     logger.info(f"Median Alignment(IG): {median_similarity_ig:.4f}")
 
 
-def other_metrics(model, dataloader, run_id, device):
-    logger = logging.getLogger(run_id)
+def sal_metrics(model, dataloader, device, run_id, use_infidelity=False, use_max_sensitivity=False, use_sparseness= False):
     model.zero_grad()
     sal = Saliency(model)
-    ig = IntegratedGradients(model)
+    logger = logging.getLogger(run_id)
 
     #init metrics: infidelity(faithfulness), max-sensitivity(robustness), sparsity(complexity)
+    if(use_max_sensitivity):
+        max_sensitivity = quantus.MaxSensitivity(nr_samples=100,
+        lower_bound=0.1,
+        norm_numerator=quantus.norm_func.fro_norm,
+        norm_denominator=quantus.norm_func.fro_norm,
+        perturb_func=quantus.perturb_func.uniform_noise,
+        similarity_func=quantus.similarity_func.difference,
+        normalise= False)
+
+    if(use_infidelity):
+        infidelity = quantus.Infidelity(
+            perturb_baseline="uniform",
+            perturb_func=quantus.perturb_func.baseline_replacement_by_indices,
+            n_perturb_samples=10,
+        )
+
+    if(use_sparseness):
+        sparseness = quantus.Sparseness()
+
+
+    sal_results = {
+        'infidelity': [],
+        'max_sensitivity': [],
+        'sparseness': []
+    }
 
 
     for images, labels in dataloader:
@@ -70,23 +95,125 @@ def other_metrics(model, dataloader, run_id, device):
         # Compute attributions
         sal_attributions = sal.attribute(
             images, target = labels
+        ).sum(axis=1).numpy()
+
+        images, labels = images.numpy(), labels.numpy()
+
+        if(use_infidelity):
+            sal_results['infidelity'].extend(
+                infidelity(model=model,
+                        x_batch=images,
+                        y_batch=labels,
+                        a_batch=sal_attributions)
+            )
+        if(use_max_sensitivity):
+            sal_results['max_sensitivity'].extend(
+                max_sensitivity(model=model,
+                                x_batch=images,
+                                y_batch=labels,
+                                a_batch=sal_attributions,
+                                explain_func=quantus.explain,
+                                explain_func_kwargs={"method": "Saliency", "softmax": False})
+            )
+        if(use_sparseness):
+            sal_results['sparseness'].extend(
+                sparseness(model=model,
+                        x_batch=images,
+                        y_batch=labels,
+                        a_batch=sal_attributions)
+            )
+
+
+    logger.info("NO. OF RESULTS:" + str(len(sal_results['infidelity'])))
+
+    logger.info("######## SALIENCY ##########")
+    if(use_infidelity):
+        logger.info(f"Mean infidelity (Saliency): {np.mean(sal_results['infidelity']):.4f}")
+        logger.info(f"Median infidelity (Saliency): {np.median(sal_results['infidelity']):.4f}")
+
+    if(use_max_sensitivity):
+        logger.info(f"Mean max-sensitivity (Saliency): {np.mean(sal_results['max_sensitivity']):.4f}")
+        logger.info(f"Median max-sensitivity (Saliency): {np.median(sal_results['max_sensitivity']):.4f}")
+
+    if(use_sparseness):
+        logger.info(f"Mean sparseness (Saliency): {np.mean(sal_results['sparseness']):.4f}")
+        logger.info(f"Median sparseness (Saliency): {np.median(sal_results['sparseness']):.4f}")
+
+
+
+def ig_metrics(model, dataloader, device, run_id, use_infidelity=False, use_max_sensitivity=False, use_sparseness= False):
+    model.zero_grad()
+    ig = IntegratedGradients(model)
+    logger = logging.getLogger(run_id)
+
+    #init metrics: infidelity(faithfulness), max-sensitivity(robustness), sparsity(complexity)
+    if(use_max_sensitivity):
+        max_sensitivity = quantus.MaxSensitivity(nr_samples=100,
+        lower_bound=0.1,
+        norm_numerator=quantus.norm_func.fro_norm,
+        norm_denominator=quantus.norm_func.fro_norm,
+        perturb_func=quantus.perturb_func.uniform_noise,
+        similarity_func=quantus.similarity_func.difference,
+        normalise= False)
+
+    if(use_infidelity):
+        infidelity = quantus.Infidelity(
+            perturb_baseline="uniform",
+            perturb_func=quantus.perturb_func.baseline_replacement_by_indices,
+            n_perturb_samples=10,
         )
-        
+
+    if(use_sparseness):
+        sparseness = quantus.Sparseness()
+
+
+    ig_results = {
+        'infidelity': [],
+        'max_sensitivity': [],
+        'sparseness': []
+    }
+
+    for images, labels in dataloader:
+        images = images.to(device)
+        labels = labels.to(device)
+        # Compute attributions
         ig_attributions = ig.attribute(
             images, baselines=torch.zeros_like(images), target=labels
+        ).numpy()
+
+        images, labels = images.numpy(), labels.numpy()
+
+        if(use_infidelity):
+            ig_results['infidelity'].extend(
+            infidelity(model=model,
+                        x_batch=images,
+                        y_batch=labels,
+                        a_batch=ig_attributions,
+                        explain_func=quantus.explain,
+                        explain_func_kwargs={"method": "IntegratedGradients", "softmax": False})
+        )
+        if(use_max_sensitivity):
+            ig_results['max_sensitivity'].extend(
+            max_sensitivity(model=model,
+                            x_batch=images,
+                            y_batch=labels,
+                            a_batch=ig_attributions)
+        )
+        if(use_sparseness):
+            ig_results['sparseness'].extend(
+            sparseness(model=model,
+                       x_batch=images,
+                       y_batch=labels,
+                       a_batch=ig_attributions)
         )
 
-        xai_methods = {
-            "Saliency": sal_attributions,
-            "IntegratedGradients": ig_attributions
-        }
 
-        results = quantus.evaluate(
-        metrics = metrics,
-        xai_methods=xai_methods,
-        model=model,
-        x_batch=images,
-        y_batch=labels,
-        **{"softmax": False,}
-    )
-    
+    logger.info("NO. OF RESULTS:" + str(len(ig_results['infidelity'])))
+
+    logger.info("######## INTEGRATED GRADIENTS ##########")
+    logger.info(f"Mean infidelity (Integrated Gradients): {np.mean(ig_results['infidelity']):.4f}")
+    logger.info(f"Median infidelity (Integrated Gradients): {np.median(ig_results['infidelity']):.4f}")
+    logger.info(f"Mean max-sensitivity (Integrated Gradients): {np.mean(ig_results['max_sensitivity']):.4f}")
+    logger.info(f"Median max-sensitivity (Integrated Gradients): {np.median(ig_results['max_sensitivity']):.4f}")
+    logger.info(f"Mean sparseness (Integrated Gradients): {np.mean(ig_results['sparseness']):.4f}")
+    logger.info(f"Median sparseness (Integrated Gradients): {np.median(ig_results['sparseness']):.4f}")
